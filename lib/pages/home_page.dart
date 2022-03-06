dc3f2e009758878345a46da281823a545fa66066
German Contreras <german.contreras@aspsa.cl>
2022-03-06 19:04:37 -0300

    diseno de app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+import 'package:stripe_app/data/tarjetas.dart';
+import 'package:stripe_app/widgets/widgets.dart';
+
+import '../helpers/helpers.dart';
+import 'pages.dart';
 
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Pagar')),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () async {
+              
+              // mostrarLoading(context);
+              // await Future.delayed(Duration(seconds: 1));
+              // Navigator.pop(context);
+
+              mostrarAlerta(context, 'titulo', 'mensaje');
+
+            },
+          )
+        ],
       ),
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
+      body: Stack(
+        children: [
+
+          Positioned(
+            width: size.width,
+            height: size.height,
+            top: 300,
+            child: PageView.builder(
+              controller: PageController(
+                viewportFraction: 0.9
+              ),
+              physics: BouncingScrollPhysics(),
+              itemCount: tarjetas.length,
+              itemBuilder: (context, index) {
+          
+              final tarjeta = tarjetas[index];
+          
+              return GestureDetector(
+                onTap: () {
+                  print(tarjeta.cardHolderName);
+
+                  Navigator.push(context, navegarFadeIn(context, TarjetaPage()));
+
+
+                },
+                child: Hero(
+                  tag: tarjeta.cardNumber,
+                  child: CreditCardWidget(
+                    cardNumber: tarjeta.cardNumber, 
+                    expiryDate: tarjeta.expiracyDate, 
+                    cardHolderName: tarjeta.cardHolderName, 
+                    cvvCode: tarjeta.cvv, 
+                    showBackView: false, 
+                    onCreditCardWidgetChange: (CreditCardBrand ) {  }, 
+                    
+                    ),
+                ),
+              );
+            }),
+          ),
+
+
+          Positioned(
+            bottom: 0,
+            child: TotalPayButton(),
+          )
+
+        ],
+      )
    );
   }
 }
--- lib/pages/tarjeta_page.dart
+++ lib/pages/tarjeta_page.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+
+import '../models/models.dart';
+import '../widgets/widgets.dart';
 
 
 class TarjetaPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
+    final tarjeta = TarjetaCredito(
+      cardNumberHidden: '4242',
+      cardNumber: '4242424242424242',
+      brand: 'visa',
+      cvv: '213',
+      expiracyDate: '01/25',
+      cardHolderName: 'Fernando Herrera'
+    );
+
+
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
+      appBar: AppBar(
+        title: Text('Pagar'),
+      ),
+      body: Stack(
+        children: [
+
+          Container(),
+
+          Hero(
+            tag: tarjeta.cardNumber,
+            child: CreditCardWidget(
+            cardNumber: tarjeta.cardNumber, 
+            expiryDate: tarjeta.expiracyDate, 
+            cardHolderName: tarjeta.cardHolderName, 
+            cvvCode: tarjeta.cvv, 
+            showBackView: false, 
+            onCreditCardWidgetChange: (CreditCardBrand ) {  }, 
+            
+            ),
+          ),
+
+          Positioned(
+            bottom: 0,
+            child: TotalPayButton(),
+          )
+        ],
+      )
    );
   }
 }
